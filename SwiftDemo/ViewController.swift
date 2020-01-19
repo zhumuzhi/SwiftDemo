1e545584574a149d9e4a15d313ea1f4af6bb829f
Zhumz <zhumuzhi@icloud.com>
2020-01-19 19:55:48 +0800

    1.语法例子

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let a = 10
+        if a>9 {
+            print(a)
+        }
+        
+        let isCan : Bool = isCanOnline(age: 22)
+        print(isCan)
+        
     }
-
+    
+    
+    func isCanOnline(age:Int) -> Bool {
+        
+        guard age>=18 else {
+            print("不能上网")
+            return false
+        }
+        
+        print("可以上网")
+        return true
+        
+    }
+    
 
 }
 
