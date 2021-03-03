1df55ae99b9f1300d3738fb24c101a853355ed51
朱慕之 <zhumz@xinfangsheng.com>
2021-03-03 22:21:48 +0800

    备注，增加练习案例

--- SwiftDemo/FunctionModule/Discover/DiscoverController.swift
+++ SwiftDemo/FunctionModule/Discover/DiscoverController.swift
@@ -4,18 +4,57 @@
 //
 //  Created by zhumuzhi on 2020/4/28.
 //  Copyright © 2020 zhumuzhi. All rights reserved.
-//
+//  发现控制器
 
 import UIKit
 
-class DiscoverController: UIViewController {
+class DiscoverController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var dataArray = ["案例1", "案例2", "案例3", "案例4"]
+    
+    
+    //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = kRandomColor();
-
+        view.backgroundColor = UIColor.white
+        view.addSubview(tableView)
     }
+    
+    //MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCellId")
+//        cell?.textLabel?.text = "Cell-\(indexPath.section)-\(indexPath.row)"
+        cell?.textLabel?.text = dataArray[indexPath.row]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        44.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        NSLog("选选中了第\(indexPath.section)-\(indexPath.row)")
+        NSLog("选选中了:\(dataArray[indexPath.row])")
+    }
+    
+    //MARK: - Lazy
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView (frame: CGRect(x: 0, y: CGFloat(kSafeAreaTopHeight), width: kScreenWidth, height: kScreenHeight - CGFloat(kSafeAreaTopHeight) - CGFloat(kSafeAreaBottomHeight)), style: .plain)
+        tableView.backgroundColor = UIColor.white
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCellId")
+        tableView.tableFooterView = UIView()                            // 分割线显示至最后一个Cell
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none  // 隐藏分割线
+        return tableView
+    }()
+    
 
 }
--- SwiftDemo/FunctionModule/Topic/TopicController.swift
+++ SwiftDemo/FunctionModule/Topic/TopicController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by zhumuzhi on 2020/4/28.
 //  Copyright © 2020 zhumuzhi. All rights reserved.
-//
+//  主题控制器
 
 import UIKit
 import Foundation
